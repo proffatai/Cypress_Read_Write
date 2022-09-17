df0ca7e12f9dcce578f7e253e4c3e0f6ae6e7cc6
proffatai <proffatai@gmail.com>
2022-09-17 17:24:21 +0100

    Rearranging the blocks

--- cypress/e2e/read2.cy.js
+++ cypress/e2e/read2.cy.js
@@ -1,16 +1,20 @@
 /// <reference types="cypress" />
 
+before(()=>{ //this will run before any other part of the script
+  
+  // we are storing the content of the json file into an object called data
+  cy.fixture('creds.json').as('data') // usual way of calling the fixtures ()
+  // alternatively
+ // cy.readFile('./cypress/fixtures/creds.json').as('data') 
+})
+
 beforeEach(() => {
-    // this will first run to open the website on each test case / it()
+    // this will rub after the before() has run to open the website on each test case / it()
 
     cy.visit("https://trytestingthis.netlify.app");
   });
 
-  before(()=>{ // we are storing the content of the json file into an object called data
-    cy.fixture('creds.json').as('data') // usual way of calling the fixtures ()
-    // alternatively
-   // cy.readFile('./cypress/fixtures/creds.json').as('data') 
-  })
+
   // Testing Valid username and valid password
   it("Read File Demo", function () {
     cy.get("#uname").type(this.data.username); // we are now using the data object to access the keys. Note, we must include the this keyword
